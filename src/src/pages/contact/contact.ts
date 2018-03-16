33fed69adddda4af8a3b5477e96b60646b22e62b
polecats <mudasdev@gmail.com>
2018-03-16 18:51:11 +0800

    Initial fork commit
    Signed-off-by: polecats <mudasdev@gmail.com>

--- src/src/pages/contact/contact.ts
+++ src/src/pages/contact/contact.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
-  constructor(public navCtrl: NavController) {
-
+  platformList: string = '';
+  isApp: boolean = true;
+  
+  constructor(public navCtrl: NavController, public platform: Platform) {
+	  let platforms = this.platform.platforms();
+	  
+	if(this.platform.is('core') || this.platform.is('mobileweb')) {
+		this.isApp = false;
+	} else {
+		this.isApp = true;
+	}
+	
+	if (this.isApp) {
+		this.myLabel = 'This is a mobile';
+	}
+	else {
+		this.myLabel = 'This is a desktop';
+	}
   }
-
 }
